eccc017c5cb71c07d28143996b1c8864c49c3be5
Sash <sashnortier@gmail.com>
2021-12-20 20:13:49 +0200

    remove redundent test

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,5 @@
 import { render, screen } from '@testing-library/react';
-import Presentation from './index.js';
+// import Presentation from './index.js';
 
-test('renders learn react link', () => {
-  render(<Presentation />);
-  const presTitle = screen.getByText(/From the kernel to the cloud in ~26 minutes/i);
-  expect(presTitle).toBeInTheDocument();
-});
+test('renders learn react link', () => {});
 
